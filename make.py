e308b276281aa6ec042293e042f19ebdb828920b
geozeke <pete@nardi.com>
2022-01-12 09:52:23 -0500

    Updated version string.

--- make.py
+++ make.py
@@ -175,7 +175,7 @@ def main():
     msg = """Perform various utility operations for a pypi development
     project."""
 
-    epi = "Version: 1.0.2"
+    epi = "Version: 1.0.4"
 
     parser = argparse.ArgumentParser(description=msg, epilog=epi)
 
